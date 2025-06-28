190ebb04e8151679402ffc4afc3cda09b0ed1b42
Petr Stroganov <petr.stroganov@gmail.com>
2025-06-28 22:08:44 +0900

    update

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -182,18 +182,20 @@ if [ "$CONNECT_TO_TESTNET" = true ]; then
     ORG_ID=$(awk 'BEGIN { FS = "\"" } !/^[ \t]*[{}]/ { print $(NF - 1); exit }' modal-login/temp-data/userData.json)
     echo "Your ORG_ID is set to: $ORG_ID"
 
-    # Wait until the API key is activated by the client
-    echo "Waiting for API key to become activated..."
-    while true; do
-        STATUS=$(curl -s "http://localhost:3000/api/get-api-key-status?orgId=$ORG_ID")
-        if [[ "$STATUS" == "activated" ]]; then
-            echo "API key is activated! Proceeding..."
-            break
-        else
-            echo "Waiting for API key to be activated..."
-            sleep 5
-        fi
-    done
+    if [ ! -f "${IDENTITY_PATH}" ]; then
+        # Wait until the API key is activated by the client
+        echo "Waiting for API key to become activated..."
+        while true; do
+            STATUS=$(curl -s "http://localhost:3000/api/get-api-key-status?orgId=$ORG_ID")
+            if [[ "$STATUS" == "activated" ]]; then
+                echo "API key is activated! Proceeding..."
+                break
+            else
+                echo "Waiting for API key to be activated..."
+                sleep 5
+            fi
+        done
+    fi
 fi
 
 echo_green ">> Getting requirements..."
