629bd25d741d521714feee9c5585c62171735616
Petr Stroganov <petr.stroganov@gmail.com>
2025-07-04 00:59:53 +0900

    update

--- entrypoint_vastai.sh
+++ entrypoint_vastai.sh
@@ -8,9 +8,11 @@ start_tunnel() {
     curl -L https://github.com/fatedier/frp/releases/download/v0.63.0/frp_0.63.0_linux_amd64.tar.gz | tar -xz --strip-components=1 -C /home/gensyn/rl_swarm/frpc-tmp
     cp /home/gensyn/rl_swarm/frpc-tmp/frpc /home/gensyn/rl_swarm/frpc/frpc
     chmod +x /home/gensyn/rl_swarm/frpc/frpc
+    export LOCAL_IP="localhost"
+    export LOCAL_PORT="3000"
     export PROXY_HASH=$(echo -n "${NODE_ID}${LOCAL_IP}${LOCAL_PORT}${NODE_PROXY_SALT}" | md5sum | cut -d ' ' -f1)
-    export PROXY_URL=${PROXY_HASH}.${NODE_PROXY_URL}
-    export NODE_PROXY_URL=${NODE_PROXY_URL}
+    export PROXY_URL="${PROXY_HASH}.${NODE_PROXY_URL}"
+    # export NODE_PROXY_URL=${NODE_PROXY_URL}
     export NODE_PROXY_PORT=${NODE_PROXY_PORT:-7000}
 
     echo -n "https://${PROXY_URL}" > /home/gensyn/rl_swarm/frpc/link
