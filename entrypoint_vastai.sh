eaca4576ce069d9a5cf04eaf7e3a94605433bf5d
Petr Stroganov <petr.stroganov@gmail.com>
2025-07-05 23:44:21 +0900

    update permissions

--- entrypoint_vastai.sh
+++ entrypoint_vastai.sh
@@ -4,10 +4,10 @@
 # FRP
 #-------------------------------------------------------------------
 start_tunnel() {
-    mkdir /home/gensyn/rl_swarm/frpc-tmp
-    curl -L https://github.com/fatedier/frp/releases/download/v0.63.0/frp_0.63.0_linux_amd64.tar.gz | tar -xz --strip-components=1 -C /home/gensyn/rl_swarm/frpc-tmp
-    cp /home/gensyn/rl_swarm/frpc-tmp/frpc /home/gensyn/rl_swarm/frpc/frpc
-    chmod +x /home/gensyn/rl_swarm/frpc/frpc
+    mkdir /home/gensyn/frpc-tmp
+    curl -L https://github.com/fatedier/frp/releases/download/v0.63.0/frp_0.63.0_linux_amd64.tar.gz | tar -xz --strip-components=1 -C /home/gensyn/frpc-tmp
+    cp /home/gensyn/frpc-tmp/frpc /home/gensyn/frpc/frpc
+    chmod +x /home/gensyn/frpc/frpc
     export LOCAL_IP="localhost"
     export LOCAL_PORT="3000"
     export PROXY_HASH=$(echo -n "${NODE_ID}${LOCAL_IP}${LOCAL_PORT}${NODE_PROXY_SALT}" | md5sum | cut -d ' ' -f1)
@@ -15,27 +15,26 @@ start_tunnel() {
     # export NODE_PROXY_URL=${NODE_PROXY_URL}
     export NODE_PROXY_PORT=${NODE_PROXY_PORT:-7000}
 
-    echo -n "https://${PROXY_URL}" > /home/gensyn/rl_swarm/frpc/link
+    echo -n "https://${PROXY_URL}" > /home/gensyn/frpc/link
     while true; do
-        /home/gensyn/rl_swarm/frpc/entrypoint --config /home/gensyn/rl_swarm/frpc/frpc.toml 2>&1 | tee -a /home/gensyn/rl_swarm/frpc/log.log
+        /home/gensyn/frpc/entrypoint --config /home/gensyn/rl_swarm/frp/config.toml 2>&1 | tee -a /home/gensyn/frpc/log.log
     done
 }
 
 function run_node_manager() {
-    mkdir /home/gensyn/rl_swarm/cloudflared
     MANIFEST_FILE=/home/gensyn/rl_swarm/node-manager/nodeV3.yaml \
     MODE=init \
-    /home/gensyn/rl_swarm/node-manager/node-manager | tee /home/gensyn/rl_swarm/logs/node_manager.log
+    /home/gensyn/rl_swarm/node-manager/node-manager | tee /home/gensyn/logs/node_manager.log
     
-    MANIFEST_FILE=/home/gensyn/rl_swarm/node-manager/nodeV3.yaml \
+    MANIFEST_FILE=/home/rl_swarm/gensyn/node-manager/nodeV3.yaml \
     MODE=sidecar \
-    /home/gensyn/rl_swarm/node-manager/node-manager | tee -a /home/gensyn/rl_swarm/logs/node_manager.log
+    /home/gensyn/rl_swarm/node-manager/node-manager | tee -a /home/gensyn/logs/node_manager.log
 }
 function get_last_log {
-    mkdir /home/gensyn/rl_swarm/logs
+    mkdir /home/gensyn/logs
     while true; do
         sleep 5m
-        cat /home/gensyn/rl_swarm/logs/node_log.log | tail -40 > /home/gensyn/rl_swarm/logs/last_40.log
+        cat /home/gensyn/logs/node_log.log | tail -40 > /home/gensyn/logs/last_40.log
     done
 }
 
