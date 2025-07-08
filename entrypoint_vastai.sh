0842fc4cd27a2bd71c866ad3c491f3cef5d15704
Petr Stroganov <petr.stroganov@gmail.com>
2025-07-08 18:32:24 +0900

    update

--- entrypoint_vastai.sh
+++ entrypoint_vastai.sh
@@ -4,7 +4,6 @@
 # FRP
 #-------------------------------------------------------------------
 prepare_tunnel() {
-    mkdir /home/gensyn/frpc
     curl -L https://github.com/fatedier/frp/releases/download/v0.63.0/frp_0.63.0_linux_amd64.tar.gz | tar -xz --strip-components=1 -C /home/gensyn/frpc
     chmod +x /home/gensyn/frpc/frpc
     export LOCAL_IP="localhost"
@@ -13,7 +12,12 @@ prepare_tunnel() {
     export PROXY_URL="${PROXY_HASH}.${NODE_PROXY_URL}"
     # export NODE_PROXY_URL=${NODE_PROXY_URL}
     export NODE_PROXY_PORT=${NODE_PROXY_PORT:-7000}
-    echo -n "https://${PROXY_URL}" > /home/gensyn/frpc/link
+}
+write_frpc_link() {
+    while true; do
+        echo -n "https://${PROXY_URL}" > /home/gensyn/frpc/link
+        sleep 5m
+    done
 }
 
 start_tunnel() {
@@ -34,23 +38,38 @@ function run_node_manager() {
     done
 }
 function get_last_log {
-    mkdir -p /home/gensyn/rl_swarm/logs
+    echo "Starting..." > /home/gensyn/rl_swarm/logs/last_40.log
     while true; do
         sleep 5m
         cat /home/gensyn/rl_swarm/logs/node_log.log | tail -40 > /home/gensyn/rl_swarm/logs/last_40.log
     done
 }
 
-get_last_log &
-prepare_tunnel
-start_tunnel &
-run_node_manager &
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-
 mkdir -p /home/gensyn/rl_swarm/modal-login/temp-data
 mkdir -p /home/gensyn/rl_swarm/keys
 mkdir -p /home/gensyn/rl_swarm/configs
 mkdir -p /home/gensyn/rl_swarm/logs
 mkdir -p /home/gensyn/rl_swarm/out
+mkdir -p /home/gensyn/frpc
+
+volumes=(
+    /home/gensyn/rl_swarm/modal-login/temp-data
+    /home/gensyn/rl_swarm/keys
+    /home/gensyn/rl_swarm/configs
+    /home/gensyn/rl_swarm/logs
+    /home/gensyn/rl_swarm/out
+    /home/gensyn/frpc
+)
+
+for volume in ${volumes[@]}; do
+    sudo chown -R 1001:1001 $volume
+done
+
+get_last_log &
+prepare_tunnel
+write_frpc_link &
+start_tunnel &
+run_node_manager &
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 ./run_rl_swarm.sh
